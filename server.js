11da8fcb624a6453694b4be0c297b1f17fe16b5e
ijazm <ijazmohamed.ijaz@gmail.com>
2017-04-30 00:02:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get("/", function(req,res) {
     res.render("index");
 })
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
    var host = server.address().address
    var port = server.address().port
    
