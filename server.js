758b3f877b97c43032bbba1a0a63e8f86c3a0877
ijazm <ijazmohamed.ijaz@gmail.com>
2017-05-01 11:58:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get("/", function(req,res) {
-    res.render("<html><head></head><body><p>Hello world I am single node js app</p></body></html>");
+    res.end("<html><head></head><body><p>Hello world I am single node js app</p></body></html>");
 })
 
 var server = app.listen(process.env.PORT || 8080, function () {
